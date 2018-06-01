d7d09f6551520e1cb66f8ae1954882035cadd0b0
unknown <hzlvtangjie@cn.net.ntes>
2018-06-01 18:23:14 +0800

    auto create results dir

--- rl_main.py
+++ rl_main.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 from pkg.config import config
 from pkg.algos import *
 
@@ -6,4 +6,6 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Usage: python rl_main.py int(workers)')
         sys.exit(0)
+    if not os.path.exists("results/"):
+        os.makedirs("results/")
     globals()[config.ALGORITHM]().run(int(sys.argv[1]))
